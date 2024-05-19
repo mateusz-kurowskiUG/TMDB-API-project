0be782b765bc3d56c105913dbe0061c63bea142c
Mateusz Kurowski <m.kurowski.023@studms.ug.edu.pl>
2024-05-19 22:31:24 +0200

    admin updating movie migrated

--- backend/src/api/routes/admin.routes.ts
+++ backend/src/api/routes/admin.routes.ts
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import { type Request, type Response, Router } from "express";
 import type IMovie from "../../interfaces/IMovie";
 import AdminDB from "../../db/admin/admin";
@@ -47,66 +48,52 @@ adminRouter.post("/movies", async (request: Request, res: Response) => {
 		overview: overview || null,
 	};
 	const movie = await AdminDB.createMovie(newMovie);
-	if (!movie.result) {
-		return res.status(400).json(movie);
-	}
+	if (!movie.result) return res.status(400).json(movie);
 
 	return res.status(200).send(movie);
 });
-adminRouter.put("/movies/:movieId", async (request: Request, res: Response) => {
-	const { movieId } = request.params;
-	if (!movieId) {
-		return res.status(400).json({ msg: "Please enter movieId" });
-	}
+adminRouter.patch(
+	"/movies/:movieId",
+	async (request: Request, res: Response) => {
+		const { movieId } = request.params;
+		if (!movieId) return res.status(400).json({ msg: "Please enter movieId" });
+		const {
+			title,
+			release_date,
+			poster_path,
+			backdrop_path,
+			popularity,
+			adult,
+			budget,
+			status,
+			TMDBId,
+			overview,
+		} = request.body;
+		const updatedMovie: IMovie = {
+			id: movieId,
+			title: title || null,
+			release_date: release_date || null,
+			poster_path: poster_path || null,
+			backdrop_path: backdrop_path || null,
+			popularity: popularity || null,
+			adult: adult || null,
+			budget: budget || null,
+			status: status || null,
+			TMDBId: TMDBId || null,
+			overview: overview || null,
+		};
+		const movieValues = _.values(updatedMovie).slice(1);
+		if (movieValues.every((value) => !value))
+			return res
+				.status(400)
+				.json({ result: false, msg: "Please add some fields to replace" });
 
-	const {
-		title,
-		release_date,
-		poster_path,
-		backdrop_path,
-		popularity,
-		adult,
-		budget,
-		status,
-		TMDBId,
-		genres,
-	} = request.body;
-	if (
-		!title ||
-		!release_date ||
-		!poster_path ||
-		!backdrop_path ||
-		!popularity ||
-		!adult === undefined ||
-		!budget ||
-		!status ||
-		TMDBId === undefined ||
-		!genres
-	) {
-		return res
-			.status(400)
-			.json({ result: false, msg: "Please enter all fields" });
-	}
+		const updated = await AdminDB.updateMovie(updatedMovie);
+		if (!updated.result) return res.status(400).json(updated);
 
-	const updatedMovie: IMovie = {
-		id: movieId,
-		title,
-		release_date,
-		poster_path,
-		backdrop_path,
-		popularity,
-		adult,
-		budget,
-		status,
-		TMDBId,
-	};
-	const updated = await AdminDB.updateMovie(movieId, updatedMovie);
-	if (!updated.result) {
-		return res.status(400).json(updated);
-	}
-
-	return res.status(200).send(updated);
-});
+		return res.status(200).send(updated);
+	},
+);
 adminRouter.delete(
 	"/movies/:movieId",
 	async (request: Request, res: Response) => {
--- backend/src/db/admin/admin.ts
+++ backend/src/db/admin/admin.ts
@@ -1,8 +1,8 @@
-import { DBMessage } from "./../../interfaces/DBResponse";
 import type IMovie from "../../interfaces/IMovie";
 import type IUser from "../../interfaces/IUser";
 import {
 	EDBMessage,
+	type IMovieUpdateResponse,
 	type IMovieCreationResponse,
 } from "../../interfaces/TDBResponse";
 import driver from "../new-connect";
@@ -18,27 +18,120 @@ const getUsers = async (): Promise<IUser[]> => {
 };
 
 const createMovie = async (movie: IMovie): Promise<IMovieCreationResponse> => {
-	const { genres, ...movieParams } = movie;
-
-	const { records } = await driver.executeQuery(
-		AdminQueries.createMovieWithoutGenres,
-		movieParams,
-	);
-	// biome-ignore lint/complexity/useLiteralKeys: no such fields as properties
-	const data = records.map((record) => record.toObject())[0]["node"][
-		// biome-ignore lint/complexity/useLiteralKeys: no such fields as properties
-		"properties"
-	];
-	console.log(data);
-
-	return {
-		result: true,
-		msg: EDBMessage.MOVIE_CREATED,
-		data,
-	};
+	const { genres, ...newMovie } = movie;
+	if (!genres || genres.length === 0) return createMovieWithoutGenres(newMovie);
+	return createMovieWithGenres(movie);
 };
 
-const updateMovie = async () => {};
+const createMovieWithGenres = async (
+	movie: IMovie,
+): Promise<IMovieCreationResponse> => {
+	try {
+		const { records } = await driver.executeQuery(
+			AdminQueries.createMovieWithGenres,
+			movie,
+		);
+		// biome-ignore lint/complexity/useLiteralKeys: no such fields as properties
+		const data = records.map((record) => record.toObject())[0]["node"][
+			// biome-ignore lint/complexity/useLiteralKeys: no such fields as properties
+			"properties"
+		];
+		return {
+			result: true,
+			msg: EDBMessage.MOVIE_CREATED,
+			data,
+		};
+	} catch (e) {
+		return {
+			result: false,
+			msg: EDBMessage.MOVIE_NOT_CREATED,
+			data: undefined,
+		};
+	}
+};
+
+const createMovieWithoutGenres = async (
+	movie: IMovie,
+): Promise<IMovieCreationResponse> => {
+	try {
+		const { records } = await driver.executeQuery(
+			AdminQueries.createMovieWithoutGenres,
+			movie,
+		);
+		// biome-ignore lint/complexity/useLiteralKeys: no such fields as properties
+		const data = records.map((record) => record.toObject())[0]["node"][
+			// biome-ignore lint/complexity/useLiteralKeys: no such fields as properties
+			"properties"
+		];
+		return {
+			result: true,
+			msg: EDBMessage.MOVIE_CREATED,
+			data,
+		};
+	} catch (e) {
+		return {
+			result: false,
+			msg: EDBMessage.MOVIE_NOT_CREATED,
+			data: undefined,
+		};
+	}
+};
+const updateMovie = async (movie: IMovie): Promise<IMovieUpdateResponse> => {
+	const { genres, ...movieParams } = movie;
+	if (!genres || genres.length === 0) return updateMovieWithoutGenres(movie);
+	return updateMovieWithGenres(movie);
+};
+
+const updateMovieWithoutGenres = async (
+	movie: IMovie,
+): Promise<IMovieUpdateResponse> => {
+	const { genres, ...movieParams } = movie;
+	try {
+		const { records } = await driver.executeQuery(
+			AdminQueries.updateMovieWithoutGenres,
+			movieParams,
+		);
+		const data = records[0].toObject()["m"]["properties"];
+		return {
+			result: true,
+			msg: EDBMessage.MOVIE_UPDATED,
+			data,
+		};
+	} catch (e) {
+		console.log(e);
+
+		return {
+			result: false,
+			msg: EDBMessage.MOVIE_NOT_UPDATED,
+			data: undefined,
+		};
+	}
+};
+
+const updateMovieWithGenres = async (
+	movie: IMovie,
+): Promise<IMovieUpdateResponse> => {
+	try {
+		const { records } = await driver.executeQuery(
+			AdminQueries.updateMovieWithGenres,
+			movie,
+		);
+		console.log(records);
+		const data = records[0].toObject()["m"]["properties"];
+
+		return {
+			result: true,
+			msg: EDBMessage.MOVIE_UPDATED,
+			data,
+		};
+	} catch (e) {
+		return {
+			result: false,
+			msg: EDBMessage.MOVIE_NOT_UPDATED,
+			data: undefined,
+		};
+	}
+};
 
 const deleteMovie = async () => {};
 const AdminDB = { getUsers, createMovie, updateMovie, deleteMovie };
