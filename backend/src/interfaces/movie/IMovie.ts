84a718175c823ed6eabedabd26698104a3dc687b
Mateusz Kurowski <m.kurowski.023@studms.ug.edu.pl>
2024-05-29 00:04:03 +0200

    fetching most popular movies from TMDB done

--- backend/src/interfaces/movie/IMovie.ts
+++ backend/src/interfaces/movie/IMovie.ts
@@ -1,15 +1,16 @@
 interface IMovie {
-	TMDBId?: number | null;
-	adult: boolean;
-	backdrop_path?: string | null;
-	budget?: number | null;
-	genres?: string[] | null;
-	id: string;
-	overview?: string | null;
-	popularity?: number | null;
-	poster_path?: string | null;
-	release_date?: string | null;
-	status?: string | null;
-	title: string;
+  TMDBId?: number | null;
+  adult: boolean;
+  backdrop_path?: string | null;
+  budget?: number | null;
+  genres?: string[] | null;
+  genres_ids?: number[] | null;
+  id: string;
+  overview?: string | null;
+  popularity?: number | null;
+  poster_path?: string | null;
+  release_date?: string | null;
+  status?: string | null;
+  title: string;
 }
 export default IMovie;
